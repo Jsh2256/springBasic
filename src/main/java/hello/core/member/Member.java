a2433c8b118a148dd95b65d3e9239e1fa2bde583
jsh2256 <jsh2256@naver.com>
2025-07-31 16:57:44 +0900

    Initial commit

---
+++ src/main/java/hello/core/member/Member.java
@@ -0,0 +1,26 @@
+package hello.core.member;
+
+public class Member {
+
+    private Long id;
+    private String name;
+    private Grade grade;
+
+    public Member(Long id, String name, Grade grade) {
+        this.grade = grade;
+        this.name = name;
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Grade getGrade() {
+        return grade;
+    }
+}
